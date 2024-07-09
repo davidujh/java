232f90762955a996e6f611624a4cfc82afcd4c4b
davidujh <wogkr8301@gmail.com>
2024-07-09 18:05:09 +0900

    awd

--- java_study/src/java0709/D0709.java
+++ java_study/src/java0709/D0709.java
@@ -7,18 +7,20 @@ public class D0709 {
 		
 		move d = new dog();
 		move c = new cat();
-		c.moving();
+		c.moving(); 
 		d.moving();
 
 	}
 
 }
 class dog implements move{
+	@Override
 	public void moving(){
 		System.out.println("성큼 성큼 이동한다");
 	}
 }
 class cat implements move{
+	@Override
 	public void moving() {
 		System.out.println("사뿐 사뿐 이동한다");
 	}
@@ -37,4 +39,10 @@ interface move{
    	- 객체 생성 불가
    	- 클레스들에 연결되어 사용된다
    	- 다향성 구현을 위해 사용한다.
+   	- 프로그램의 기본틀을 구축할 수 있다.
+   	
+   	
+   	서버에서 클라이언트에게 페이지를 제공한다
+   	클라이 언트의 요청을 받아주는 메서드
+   	클라이언트가 원하는 페이지를 제공하는 메서드  
 */
--- java_study/src/java0709/D0709_3.java
+++ java_study/src/java0709/D0709_3.java
@@ -1,10 +1,80 @@
 package java0709;
 
+interface RestCustomer{// 식당에 찾아오는 손님
+	 //예약손님
+	public void setCustomer(String customer);
+	public String food();
+}
+
+class ReservCustomerA implements RestCustomer{	
+	String customer;
+
+	@Override
+	public void setCustomer(String customer) {
+		this.customer =customer;
+		
+	}
+
+	@Override
+	public String food() {
+		return customer+ "청국장 준다";
+		
+	}
+}//예약손님 A 
+class ReservCustomerB implements RestCustomer{	
+	String customer;
+
+	@Override
+	public void setCustomer(String customer) {
+		this.customer=customer;
+		
+	}
+
+	@Override
+	public String food() {
+		return customer+"돈가스 준다";
+		
+	}
+}//예약손님 B
+
+class Owner {//사장
+	void food(ReservCustomerA cusA) {
+		System.out.println(cusA.customer + "청국장 준다");
+	}
+	void food(ReservCustomerB cusB) {
+		System.out.println(cusB.customer + "돈가스 준다");
+	}
+}
+
+
+
+
 public class D0709_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Owner ow = new Owner();
+		ReservCustomerA a = new ReservCustomerA();
+		ReservCustomerB b = new ReservCustomerB();
+		a.setCustomer("A-이순신");
+		b.setCustomer("B-장보고");
+		
+		ow.food(a);
+		ow.food(b);
+		
 	}
 
 }
+
+
+// 초등학생 , 중학생 , 고등학생 , 대학생
+
+
+
+//사용자 요청 처리 클레스ㅡ - 컨트롤 클래스
+// 사용자 요청에 필요한 데이터 가공 및 수집 실핼 클래스 - 서비스 클래스
+// 데이터만 저장하는 틀래스 - 사용자에게 제공할 데이터 클래스 - VO,DTO
+//					  - 데이터베이스에 저장할 데이터 클래스 - Entity
+
+// 데이터베이스 처리 인터페이스 - repository
